b14d283f6ddff5c40045802f11d31a29b23a1cf6
Snow <dawooda0104@gmail.com>
2024-01-30 02:31:51 +0000

    Added empty test functions for Menu endpoint.

--- backend/endpoints/menu_test.go
+++ backend/endpoints/menu_test.go
@@ -6,6 +6,26 @@ import (
 	"testing"
 )
 
-func TestFetchMenu(t *testing.T) {
+func TestFetchMenu1(t *testing.T) {
+	// Fetch all items on the menu
+}
 
+func TestFetchMenu2(t *testing.T) {
+	// Fetch all items that have x calories
+}
+
+func TestFetchMenu3(t *testing.T) {
+	// Fetch all items that do NOT have x allergen
+}
+
+
+
+func TestChangeMenu1(t *testing.T) {
+	// Change item with given name to have a slightly different name, should return 200 Okay
+}
+func TestChangeMenu2(t *testing.T) {
+	// Change name item with given ID, should return 200 Okay if exists and 404 if not
+}
+func TestChangeMenu3(t *testing.T) {
+	// Change item with given ID to have different ID, should return 403
 }
