b789bd004a83d8b5cd3e078605a1fe12b129a8bf
Jacob <Jacob.Padley.2022@live.rhul.ac.uk>
2024-01-30 16:44:56 +0000

    Refactor tests to use Go bulk tests

--- backend/database/dbconn_test.go
+++ backend/database/dbconn_test.go
@@ -3,43 +3,53 @@
 package database
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(m *testing.M) {
+func TestDatabaseQueries(t *testing.T) {
 	// Setup test data
 	UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD', 5.00, 400)")
 	UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD2', 6.00, 500)")
 	UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD3', 7.00, 600)")
 	UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD4', 8.01, 720)")
 
-	code := m.Run()
+	testCases := []struct {
+		name            string
+		filter          *MenuFilter
+		expectedLen     int
+		expectedElement MenuItem
+	}{
+		{
+			name:            "EmptyFilter",
+			filter:          &MenuFilter{},
+			expectedLen:     4,
+			expectedElement: MenuItem{MenuItemId: 1, ItemName: "TESTFOOD", Price: 5.00, Calories: 400},
+		},
+		{
+			name:            "WithSearchTermFilter",
+			filter:          &MenuFilter{SearchTerm: "TESTFOOD2"},
+			expectedLen:     1,
+			expectedElement: MenuItem{MenuItemId: 2, ItemName: "TESTFOOD2", Price: 6.00, Calories: 500},
+		},
+		{
+			name:            "WithMultipleFilters",
+			filter:          &MenuFilter{MaxPrice: 6.00, MaxCalories: 600},
+			expectedLen:     2,
+			expectedElement: MenuItem{MenuItemId: 1, ItemName: "TESTFOOD", Price: 5.00, Calories: 400},
+		},
+	}
 
-	// Remove test values after tests
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			result := QueryMenu(test.filter)
+			assert.Equal(t, test.expectedLen, len(result), "Check query returns correct number of items")
+			assert.Contains(t, result, test.expectedElement, "Check that query result contains expected item")
+		})
+	}
+
 	UpdateDB("DELETE FROM menuitem")
-
-	os.Exit(code)
-}
-
-func TestGetMenu(t *testing.T) {
-	menu := QueryMenu(&MenuFilter{})
-	assert.Equal(t, 4, len(menu), "Number of menu items should be 4")
-	assert.Contains(t, menu, MenuItem{MenuItemId: 1, ItemName: "TESTFOOD", Price: 5.00, Calories: 400}, "Test that the correct menu items are returned")
-}
-
-func TestGetMenuWithArgs(t *testing.T) {
-	items := QueryMenu(&MenuFilter{SearchTerm: "TESTFOOD2"})
-	assert.Equal(t, 1, len(items), "Query should only return one item")
-	assert.Equal(t, "TESTFOOD2", items[0].ItemName, "Test correct item is returned from query")
-}
-
-func TestGetMenuWithMultipleArgs(t *testing.T) {
-	items := QueryMenu(&MenuFilter{MaxPrice: 6.00, MaxCalories: 600})
-	assert.Equal(t, 2, len(items), "Query should return two items")
-	assert.Contains(t, items, MenuItem{MenuItemId: 1, ItemName: "TESTFOOD", Price: 5.00, Calories: 400}, "Test that the correct menu items are returned")
 }
 
 func TestDBAuth(t *testing.T) {
--- backend/endpoints/menu_test.go
+++ backend/endpoints/menu_test.go
@@ -10,41 +10,48 @@ import (
 )
 
 func TestFetchMenu(t *testing.T) {
-	filter := &database.MenuFilter{}
-	_, err := FetchMenu(filter)
-	assert.NoError(t, err)
-}
+	// Setup test data
+	database.UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD', 5.00, 400)")
+	database.UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD2', 6.00, 500)")
+	database.UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD3', 7.00, 600)")
+	database.UpdateDB("INSERT INTO menuitem (itemname, price, calories) VALUES ('TESTFOOD4', 8.01, 720)")
 
-func TestFetchMenuWithNameFilter(t *testing.T) {
-	filter := &database.MenuFilter{
-		SearchTerm: "2",
+	testCases := []struct {
+		name   string
+		filter *database.MenuFilter
+	}{
+		{
+			name:   "EmptyFilter",
+			filter: &database.MenuFilter{},
+		},
+		{
+			name:   "WithSearchTermFilter",
+			filter: &database.MenuFilter{SearchTerm: "2"},
+		},
+		{
+			name:   "WithPriceFilter",
+			filter: &database.MenuFilter{MaxPrice: 5.00},
+		},
+		{
+			name:   "WithCalorieFilter",
+			filter: &database.MenuFilter{MaxCalories: 500},
+		},
+		{
+			name: "WithMultipleFilters",
+			filter: &database.MenuFilter{
+				SearchTerm:  "TESTFOOD",
+				MaxPrice:    6.00,
+				MaxCalories: 600,
+			},
+		},
 	}
-	_, err := FetchMenu(filter)
-	assert.NoError(t, err)
-}
 
-func TestFetchMenuWithPriceFilter(t *testing.T) {
-	filter := &database.MenuFilter{
-		MaxPrice: 5.00,
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := FetchMenu(test.filter)
+			assert.NoError(t, err)
+		})
 	}
-	_, err := FetchMenu(filter)
-	assert.NoError(t, err)
-}
 
-func TestFetchMenuWithCalorieFilter(t *testing.T) {
-	filter := &database.MenuFilter{
-		MaxCalories: 500,
-	}
-	_, err := FetchMenu(filter)
-	assert.NoError(t, err)
-}
-
-func TestFetchMenuWithMultipleFilters(t *testing.T) {
-	filter := &database.MenuFilter{
-		SearchTerm:  "TESTFOOD",
-		MaxPrice:    6.00,
-		MaxCalories: 600,
-	}
-	_, err := FetchMenu(filter)
-	assert.NoError(t, err)
+	database.UpdateDB("DELETE FROM menuitem")
 }
