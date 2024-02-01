ec30d42123bd3ab7c72d6b38805de663d17ebde0
Jacob <Jacob.Padley.2022@live.rhul.ac.uk>
2024-02-01 11:00:27 +0000

    Add tests for RemoveItem

--- backend/endpoints/remove_item.go
+++ backend/endpoints/remove_item.go
@@ -1,1 +1,7 @@
 package endpoints
+
+import "github.com/gofiber/fiber/v2"
+
+func RemoveItem(c *fiber.Ctx) error {
+	return nil
+}
---
+++ backend/endpoints/remove_item_test.go
@@ -0,0 +1,45 @@
+//go:build integration
+
+package endpoints
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveItem(t *testing.T) {
+	app := fiber.New()
+	app.Delete("/remove_item", RemoveItem)
+
+	testCases := []struct {
+		name string
+		id   string
+		code int
+	}{
+		{
+			name: "WithCorrectFields",
+			id:   "1",
+			code: 200,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			// Create a new HTTP request
+			req, _ := http.NewRequest("DELETE", "/remove_item", nil)
+			q := req.URL.Query()
+			q.Add("itemId", test.id)
+			req.URL.RawQuery = q.Encode()
+
+			res, err := app.Test(req)
+			assert.NoError(t, err)
+			defer res.Body.Close()
+
+			// Check the response
+			assert.Equal(t, test.code, res.StatusCode, "Check that request returned expected status code")
+		})
+	}
+}
