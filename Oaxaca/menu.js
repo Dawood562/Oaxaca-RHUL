0cf78fe54b37050f67503f8f130965283ad3fe9a
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-02-05 17:55:38 +0000

    Added basic editing function. Now able to edit an existing menu item and submit it to backend

--- Oaxaca/menu.js
+++ Oaxaca/menu.js
@@ -9,15 +9,21 @@ function initMenuAll(){
 
     let data = requestMenu(0,0,0); // Zero value = none specified
     data.then(r => {
+        console.log(r)
         currentMenu = r
         let index = 0;
         document.getElementById("menuSectionAll").innerHTML = ""
         r.forEach(element => {
-            let item = '<li id="item'+index+'" class="genericMenuItem" data-calories="'+element.calories+'" data-price="'+element.price+'">'+element.itemName+' -- £'+element.price.toFixed(2)+' -- '+element.calories+'kcal</li>';
-            document.getElementById("menuSectionAll").innerHTML+= item
+            document.getElementById("menuSectionAll").innerHTML+= createMenuItem(index, element.itemName, element.price, element.calories);
             index++;
         });
     })
+}
+
+// Function that takes in data and turns into menu item to be displayed
+function createMenuItem(index, itemName, price, calories){
+    let comp = '<li id="item'+index+'" class="genericMenuItem" data-calories="'+calories+'" data-price="'+price+'">'+itemName+' -- £'+price.toFixed(2)+' -- '+calories+'kcal</li>'
+    return comp;
 }
 
 // Toggle edit mode
@@ -26,13 +32,84 @@ function editMenu(){
     if(!editMode){
         document.getElementById("menuSectionAll").innerHTML += "<div id='newItemDiv'><h3>Add new menu item:</h3><p><label>Name:</label><input type='text' id='newItemNameField'>  <label>Price:</label><input type='text' id='newItemPriceField'>  <label>Calories:</label><input type='text' id='newItemCaloriesField'>    <button onclick='addMenuItem()'>+</button></p></div>";
         document.getElementById("menuSectionAll").innerHTML += "<div id='removeItemDiv'><h3>Delete menu item:</h3><p><label>Enter the name of the item you want to delete from the menu:</label><input type='text' id='deleteItemNameField'> <button onclick='deleteMenuItem()'>-</button></p></div>";
+        
+        // Add edit button to each menu item
+        for(let i = 0; i < currentMenu.length; i++){
+            document.getElementById("item"+i).innerHTML += "<button index='"+i+"' class='editMenuItem'>EDIT</button>"
+        }
+
+        // Retrieve all edit buttons into iterable
+        const editButtons = document.querySelectorAll(".editMenuItem")
+
+        // For each edit button add click listener
+        editButtons.forEach(item => {
+            item.addEventListener('click', function(){
+                toggleEditMode(item.getAttribute("index"))
+            })
+        })
+
         editMode = true;
     }else{
         document.getElementById("newItemDiv").remove();
         document.getElementById("removeItemDiv").remove();
+        const editButtons = document.querySelectorAll('.editMenuItem');
+        editButtons.forEach(item => {
+            item.remove();
+        })
         editMode = false;
     }
 }
+
+
+// Holds global variable on if a field is currently being edited
+var editingText = false;
+// Holds location of item being edited
+var currentlyEditing = -1;
+var oldText
+
+// From here a textfield should appear where menu item was with the previous content that was there inside it
+// Also submit button should apear instead of 
+function toggleEditMode(index){
+    let previousText = document.getElementById("item"+index).textContent;
+
+    previousText = previousText.substring(0,previousText.length-4); // Gets rid of edit
+
+    document.getElementById("item"+index).innerHTML = "<input id='menuEditBox' type='text' value='"+previousText+"'> <button id='menuEditSubmitButton'>submit</button>"
+    document.getElementById("menuEditSubmitButton").addEventListener('click', function (){
+        submitMenuEdit(index);
+    });
+
+    // Must now replace new item with item from server
+}
+
+// Needs to take in index so that it can get id
+async function submitMenuEdit(index){
+    let userInput = document.getElementById("menuEditBox").value
+    userInput = userInput.split("--");
+    let id = currentMenu[index].itemId;
+    let name = userInput[0].substring(0,userInput[0].length-1);
+    let itemPrice = parseFloat(userInput[1].substring(2, userInput[1].length-1));
+    let itemCalories = parseInt(userInput[2].substring(1, userInput[2].length-4));
+    console.log(id+","+name+","+itemPrice+","+itemCalories);
+
+    try{
+        let response = await fetch("http://localhost:4444/edit_item", {
+            method:'PATCH',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                itemId: id,
+                itemName: name,
+                price: itemPrice,
+                calories: itemCalories
+            })
+        })
+    }catch(error){
+        console.error(error);
+    }
+}
+
 // Functions to add and delete menu items
 function addMenuItem(){
     let nameValue = document.getElementById("newItemNameField").value;
