f36415691da2b4fa072cccc9882dc50e6ec0ab9e
Jacob Padley <Jacob.Padley.2022@live.rhul.ac.uk>
2024-02-06 14:18:59 +0000

    Add initial real tests and functionality for /notifications

--- backend/endpoints/notifications.go
+++ backend/endpoints/notifications.go
@@ -1,8 +1,38 @@
 package endpoints
 
-import "github.com/gofiber/contrib/websocket"
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gofiber/contrib/websocket"
+)
 
 // NewConnection registers a new connection to the system given its websocket connection
-func NewConnection(ws *websocket.Conn) {
+func NewConnection(ws *websocket.Conn) error {
+	_, m, err := ws.ReadMessage()
+	if err != nil {
+		return err
+	}
+	t := getTableNum(string(m))
+	if t == -1 {
+		return ws.WriteMessage(websocket.TextMessage, []byte("DENIED"))
+	}
+	return ws.WriteMessage(websocket.TextMessage, []byte("WELCOME"))
+}
 
+// getTableNum extracts a table number from a message or returns -1 if the message is invalid
+func getTableNum(m string) int {
+	segments := strings.Split(string(m), ":")
+	if len(segments) != 2 {
+		return -1
+	}
+	if segments[0] != "CUSTOMER" {
+		return -1
+	}
+	// Extract the table number
+	ret, err := strconv.ParseInt(segments[1], 10, 32)
+	if err != nil {
+		return -1
+	}
+	return int(ret)
 }
--- backend/endpoints/notifications_test.go
+++ backend/endpoints/notifications_test.go
@@ -4,7 +4,6 @@ package endpoints
 
 import (
 	"testing"
-	"time"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
@@ -13,39 +12,69 @@ import (
 )
 
 func TestNotifications(t *testing.T) {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
 
 	app.Get("/notifications", websocket.New(func(c *websocket.Conn) {
-		c.WriteMessage(websocket.TextMessage, []byte("Hello Client"))
+		if NewConnection(c) != nil {
+			c.Close()
+		}
 	}))
 
 	go func() {
 		app.Listen(":4444")
 	}()
-	// Wait for server to start
-	time.Sleep(5 * time.Second)
 
 	testCases := []struct {
-		name   string
-		params map[string]string
+		name string
+		msg  string
+		resp string
 	}{
 		{
-			name: "TemporaryTestCase",
-			params: map[string]string{
-				"table_num": "5",
-			},
+			name: "CustomerWithCorrectTableNumber",
+			msg:  "CUSTOMER:4",
+			resp: "WELCOME",
+		},
+		{
+			name: "CustomerWithNoTableNumber",
+			msg:  "CUSTOMER:",
+			resp: "DENIED",
+		},
+		{
+			name: "CustomerWithInvalidTableNumber",
+			msg:  "CUSTOMER:A",
+			resp: "DENIED",
+		},
+		{
+			name: "CustomerWithNoTableNumberSeparator",
+			msg:  "CUSTOMER",
+			resp: "DENIED",
+		},
+		{
+			name: "InvalidIdentifier",
+			msg:  "TEST",
+			resp: "DENIED",
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			// Create a test websocket connection
+			retries := 3
 			ws, _, err := gwebsocket.DefaultDialer.Dial("ws://127.0.0.1:4444/notifications", nil)
+			for retries > 0 && err != nil {
+				ws, _, err = gwebsocket.DefaultDialer.Dial("ws://127.0.0.1:4444/notifications", nil)
+				retries -= 1
+			}
 			assert.NoError(t, err, "Test that creating a websocket connection does not create an error")
 			defer ws.Close()
-			// Try reading from the websocket
-			_, _, err = ws.ReadMessage()
-			assert.NoError(t, err, "Test that receiving a message from the websocket does not create an error")
+
+			err = ws.WriteMessage(gwebsocket.TextMessage, []byte(test.msg))
+			assert.NoError(t, err, "Test sending a message does not create an error")
+			_, m, err := ws.ReadMessage()
+			assert.NoError(t, err, "Test that receiving a message does not create an error")
+			assert.Equal(t, test.resp, string(m), "Test that server replied with expected response")
 		})
 	}
 
