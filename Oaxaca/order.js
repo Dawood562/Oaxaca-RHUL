05b7b6f61d8737cdfd26200fb31aceb7f9a9e6e1
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-02-14 15:13:11 +0000

    Added fundamental websocket behaviour

--- Oaxaca/order.js
+++ Oaxaca/order.js
@@ -1,8 +1,43 @@
 var sock
+var sockInit = false
 
-document.addEventListener("DOMContentLoaded", function(){
-    sock = new WebSocket("ws://localhost:4444/notifications")
-    sock.addEventListener("open", e =>{
-        console.log("Connected to backend websocket")
-    })
+document.addEventListener('DOMContentLoaded', e=>{
+    initSock()
 })
+
+// Called when page is initialised and initialises websocket connection
+function initSock(){
+    sock = new WebSocket("ws://localhost:4444/notifications")
+    sock.onerror = function(event){
+        // If unsuccessfully connected
+        alert("Unsuccessfully to connect to backend websocket 🖥️🔥")
+    }
+    sock.addEventListener("open", e =>{
+        // WE NEED TO ADD A WAY TO GET USERS TABLE NUMBER
+        sock.send("CUSTOMER:7")
+    })
+
+    sock.addEventListener("message", e => handleMessages(e))
+}
+
+// Displays connection and communication status to console
+function handleMessages(e){
+    if (e.data == "WELCOME"){
+        console.log("Connected to backend websocket")
+    }else if (e.data == "OK"){
+        console.log("Notification successfully received")
+    }else{
+        console.log(e) // Display entire message if something went wrong for debugging
+    }
+}
+
+// Notifies backend a customer needs help
+function notifyHelp(){
+    sock.send("HELP")
+}
+
+// Notifies backend a customer has ordered something
+function notifyNew(){
+    sock.send("NEW")
+}
+
