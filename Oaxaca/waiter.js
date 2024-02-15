6837e182949dec569b5804eb8f670413ce3bbc8d
Jacob Padley <Jacob.Padley.2022@live.rhul.ac.uk>
2024-02-15 16:46:58 +0000

    Add basic order fetching functionality

--- Oaxaca/waiter.js
+++ Oaxaca/waiter.js
@@ -2,7 +2,8 @@ var sock
 var sockInit = false
 
 document.addEventListener('DOMContentLoaded', e=>{
-    initSock()
+    initSock();
+    refreshOrders();
 })
 
 function initSock(){
@@ -26,14 +27,39 @@ function handleMessages(e){
         console.log("Notification successfully received");
     }else if (e.data == "SERVICE"){
         // NOTIFICATION SENT BY KITCHEN STAFF TO WAITERS - DO STUFF HERE
+        alert("Kitchen has called service!")
     }else if (e.data.includes("HELP")){
         // NOTIFICATION SENT BY CUSTOMER TO WAITERS - CUSTOMER IS AT TABLE 'tableNumber' - DO STUFF BELOW
         let tableNumber = e.data.split(":")[1]
+        alert("Table " + tableNumber + " needs help!")
     }else if(e.data == "NEW"){
-        // NOTIFICATION TO WAITER WHEN CUSTOMER HAS PLACED ORDER
+        refreshOrders();
     }else{
         console.log(e); // Display entire message if something went wrong for debugging
     }
+}
+
+async function refreshOrders(){
+    let response = await fetch("http://localhost:4444/orders")
+    let data = await response.json()
+    for(var order of data) {
+        document.getElementById("order_table").innerHTML += createOrder(order)
+    }
+}
+
+function createOrder(order) {
+    let items = order.items.map((x) => x.itemId.itemName);
+    let itemsStr = "";
+    for(var item of items) {
+        itemsStr += item + ", "
+    }
+    return `<tr>
+        <td>${order.tableNumber}</td>
+        <td>${itemsStr.substring(0, itemsStr.length - 2)}</td>
+        <td>${order.status}</td>
+        <td><button type="submit">Cancel Order</button></td>
+        <td><button type="submit">Complete</button></td>
+    </tr>`;
 }
 
 function notifyConfirmation(){
