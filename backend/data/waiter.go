c12b73a27e9b4390bfaf8a3f5d1b5634cc5c7922
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-02-22 19:57:16 +0000

    Added remove function for waiters and respective unit test

--- backend/data/waiter.go
+++ backend/data/waiter.go
@@ -1,6 +1,6 @@
 package data
 
-var ActiveWaiters []Waiter
+var activeWaiters []Waiter
 
 type Waiter struct {
 	ID          uint   `json:"id"`
@@ -8,6 +8,17 @@ type Waiter struct {
 	TableNumber []uint `json:"tableNumber"` // Array of table numbers that the waiter is attending
 }
 
+// Add waiter to current waiter list
 func AddWaiter(waiter Waiter) {
-	ActiveWaiters = append(ActiveWaiters, waiter)
+	activeWaiters = append(activeWaiters, waiter)
+}
+
+// Remove waiter according to id provided in filter waiter
+func RemoveWaiter(waiter Waiter) {
+	for index, w := range activeWaiters {
+		// If requested waiter is in active waiter list then remove it
+		if waiter.ID == w.ID {
+			activeWaiters = append(activeWaiters[:index], activeWaiters[index+1:]...)
+		}
+	}
 }
--- backend/data/waiter_test.go
+++ backend/data/waiter_test.go
@@ -11,5 +11,22 @@ import (
 func TestAddWaiter(t *testing.T) {
 	tableNumbers := []uint{1, 2, 3}
 	AddWaiter(Waiter{ID: 69, Username: "John", TableNumber: tableNumbers})
-	assert.Equal(t, 1, len(ActiveWaiters), "Adding a waiter should increase number of waiters in list by 1")
+	assert.Equal(t, 1, len(activeWaiters), "Adding a waiter should increase number of waiters in list by 1")
+}
+
+func TestRemoveWaiter(t *testing.T) {
+	removePreviousWaiterData()
+	tableNumbers := []uint{1, 2, 3}
+	waiter1 := Waiter{ID: 1, Username: "John", TableNumber: tableNumbers}
+	waiter2 := Waiter{ID: 2, Username: "James", TableNumber: tableNumbers}
+	AddWaiter(waiter1)
+	AddWaiter(waiter2)
+	assert.Equal(t, 2, len(activeWaiters), "Adding two waiters should return active waiter list of 2")
+	RemoveWaiter(Waiter{ID: 1})
+	assert.Equal(t, 1, len(activeWaiters), "Removing a waiter should return a active waiter list of 1")
+	assert.Equal(t, 2, int(activeWaiters[0].ID), "Remaining waiter does not match expected waiter. Incorrect waiter removed!")
+}
+
+func removePreviousWaiterData() {
+	activeWaiters = []Waiter{} // Replace old waiter list with empty one
 }
