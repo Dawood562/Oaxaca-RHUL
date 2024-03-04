07732e8f34dc90a49159dcce6fbe5a9e6665f079
Jacob Padley <Jacob.Padley.2022@live.rhul.ac.uk>
2024-03-04 15:09:04 +0000

    Add remaining tests for /delivered

--- backend/endpoints/delivered.go
+++ backend/endpoints/delivered.go
@@ -1,7 +1,36 @@
 package endpoints
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"strconv"
+	"teamproject/database"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Delivered(c *fiber.Ctx) error {
+	// Retrieve required ID arg
+	idStr := c.Params("id")
+	if len(idStr) == 0 {
+		return fiber.ErrNotFound
+	}
+
+	id, err := strconv.ParseInt(idStr, 10, 32)
+	if err != nil {
+		return fiber.ErrUnprocessableEntity
+	}
+
+	// Attempt to mark order as delivered
+	err = database.DeliverOrder(uint(id))
+
+	if err != nil {
+		if err == database.ErrOrderAlreadyDelivered || err == database.ErrOrderAlreadyCancelled {
+			return fiber.ErrConflict
+		}
+		return fiber.ErrNotFound
+	}
+
+	BroadcastToWaiters("REFRESH")
+	// TODO: send to the customer
+
 	return nil
 }
