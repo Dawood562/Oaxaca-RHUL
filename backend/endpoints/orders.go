6265918572b4635f8da6b46a27a26cccde888ce9
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-03-05 12:23:38 +0000

    Orders now able to be filtered by table number through api call

--- backend/endpoints/orders.go
+++ backend/endpoints/orders.go
@@ -3,6 +3,7 @@ package endpoints
 import (
 	"strconv"
 	"teamproject/database"
+	"teamproject/database/models"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -20,17 +21,28 @@ func GetOrders(c *fiber.Ctx) error {
 			c.SendString(err.Error())
 		}
 		confirmed = temp
-	} else if len(onlyTableNumber) > 0 {
+	}
+
+	data, err := database.FetchOrders(confirmed, -1)
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+
+	if len(onlyTableNumber) > 0 {
 		temp, err := strconv.ParseInt(onlyTableNumber, 10, 32)
 		if err != nil {
 			c.SendString(err.Error())
 		}
 		tableNumber = int(temp)
-	}
 
-	data, err := database.FetchOrders(confirmed, tableNumber)
-	if err != nil {
-		return c.SendString(err.Error())
+		filteredData := []models.Order{}
+		// Find data for given table number
+		for _, dataItem := range data {
+			if dataItem.TableNumber == uint(tableNumber) {
+				filteredData = append(filteredData, *dataItem)
+			}
+		}
+		return c.JSON(filteredData)
 	}
 
 	return c.JSON(data)
