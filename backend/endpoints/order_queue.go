199edd2dc1f8e3928d0acbd8637f506dc4510fae
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-03-06 15:52:17 +0000

    Added function for appending orders to order queue

--- backend/endpoints/order_queue.go
+++ backend/endpoints/order_queue.go
@@ -4,8 +4,14 @@ import "teamproject/database/models"
 
 var orderQueue []models.Order
 
+// Returns true if any orders have been placed on queue
 func IsAnyPendingOrders() bool {
 	return len(orderQueue) > 0
+}
+
+// Adds provided order to queue to be given to a waiter
+func AddOrderToQueue(order models.Order) {
+	orderQueue = append(orderQueue, order)
 }
 
 func clearOrderQueue() {
--- backend/endpoints/order_queue_test.go
+++ backend/endpoints/order_queue_test.go
@@ -1,6 +1,7 @@
 package endpoints
 
 import (
+	"teamproject/database/models"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +10,12 @@ import (
 func TestPendingOrdersEmpty(t *testing.T) {
 	clearOrderQueue()
 	assert.False(t, IsAnyPendingOrders(), "Pending orders should return false when no items in pending order queue")
+}
+
+func TestOrderAddedCorrectly(t *testing.T) {
+	clearOrderQueue()
+	assert.False(t, IsAnyPendingOrders(), "Pending orders should return false when no items in pending order queue")
+	testOrder1 := models.Order{ID: 1}
+	AddOrderToQueue(testOrder1)
+	assert.True(t, IsAnyPendingOrders(), "Pending orders should return true when an item is added to the order queue")
 }
