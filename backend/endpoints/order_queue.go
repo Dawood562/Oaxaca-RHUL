14fb97f4cdf600f1a84c28b9dcb4acc3a6a28816
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2024-03-06 16:04:56 +0000

    Added duplicate detection when adding an order to order queue

--- backend/endpoints/order_queue.go
+++ backend/endpoints/order_queue.go
@@ -1,6 +1,9 @@
 package endpoints
 
-import "teamproject/database/models"
+import (
+	"errors"
+	"teamproject/database/models"
+)
 
 var orderQueue []models.Order
 
@@ -10,8 +13,19 @@ func IsAnyPendingOrders() bool {
 }
 
 // Adds provided order to queue to be given to a waiter
-func AddOrderToQueue(order models.Order) {
-	orderQueue = append(orderQueue, order)
+func AddOrderToQueue(order models.Order) error {
+	existing := false
+	for _, o := range orderQueue {
+		if order.ID == o.ID {
+			existing = true
+		}
+	}
+	if !existing {
+		orderQueue = append(orderQueue, order)
+		return nil
+	} else {
+		return errors.New("Order with same id already in queue")
+	}
 }
 
 func clearOrderQueue() {
--- backend/endpoints/order_queue_test.go
+++ backend/endpoints/order_queue_test.go
@@ -19,3 +19,11 @@ func TestOrderAddedCorrectly(t *testing.T) {
 	AddOrderToQueue(testOrder1)
 	assert.True(t, IsAnyPendingOrders(), "Pending orders should return true when an item is added to the order queue")
 }
+
+func TestDuplicateOrderThrowsError(t *testing.T) {
+	clearOrderQueue()
+	testOrder1 := models.Order{ID: 1}
+	testOrder2 := models.Order{ID: 1}
+	assert.NoError(t, AddOrderToQueue(testOrder1), "Adding an order shouldnt throw an error")
+	assert.Error(t, AddOrderToQueue(testOrder2), "Adding a duplicate order should throw an error")
+}
