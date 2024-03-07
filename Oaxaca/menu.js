c0a72d51d1f7a37de8d69b51be5e1b3ece475c42
s <zlac244>
2024-03-07 14:05:33 +0000

    Resdesign of menu

--- Oaxaca/menu.js
+++ Oaxaca/menu.js
@@ -21,10 +21,19 @@ function initMenuAll() {
 
 // Function that takes in data and turns into menu item to be displayed
 function createMenuItem(index, id, itemName, price, calories) {
-  let comp = "<div class='MenuItemDiv' id='item" + index + "'> <img class='MenuItemImg' src='image/foodimg.jpg'><br> <div class='MenuItemDetails'><label class='MenuItemName'>" + itemName + "</label><br><label class='MenuItemPrice'>£" + price.toFixed(2) + "</label><label class='MenuItemCalories'>" + calories + "kcal</label></div>";
-  comp += "<input type='number' id='itemQuantityInput" + index + "' min='1' value='1' class='itemQuantityInput'>";
-  comp += "<button id='addToBasketButton" + index + "' onclick='addToBasket(" + index + ", " + id + ", \"" + itemName + "\", " + price + ", " + calories + ")'>Add to Basket</button></a></div>";
-
+  let comp = `
+    <div class='MenuItemDiv' id='item${index}'>
+      <img class='MenuItemImg' src='image/foodimg.jpg'><br>
+      <div class='MenuItemDetails'>
+        <label class='MenuItemName'>${itemName}</label><br>
+        <label class='MenuItemPrice'>£${price.toFixed(2)}</label><br>
+        <label class='MenuItemCalories'>${calories} kcal</label><br>
+        <label class='MenuItemAllergen'>Contains</label>
+      </div>
+      <input type='number' id='itemQuantityInput${index}' min='1' value='1' class='itemQuantityInput'>
+      <button id='addToBasketButton${index}' onclick='addToBasket(${index}, ${id}, "${itemName}", ${price}, ${calories})'>Add to Basket</button>
+    </div>
+  `;
   return comp;
 }
 
@@ -38,7 +47,7 @@ function editMenu() {
     // Add edit button to each menu item
     for (let i = 0; i < currentMenu.length; i++) {
       document.getElementById("item" + i).innerHTML += "<button index='" + i + "' id='itemEditButton' class='editMenuItem'>EDIT</button>"
-      let thing = document.getElementById("item"+i).childNodes[5].remove()
+      let thing = document.getElementById("item" + i).childNodes[5].remove()
     }
 
     // Retrieve all edit buttons into iterable
@@ -46,7 +55,7 @@ function editMenu() {
 
     // For each edit button add click listener
     editButtons.forEach(item => {
-      item.addEventListener('click', function() {
+      item.addEventListener('click', function () {
         EditMenuField(item.getAttribute("index"))
       })
     })
@@ -86,19 +95,19 @@ function EditMenuField(index) {
 
   document.getElementById("itemEditButton").remove();
   document.getElementById("item" + index).innerHTML += "<button index='" + index + "' id='itemEditButton32' class='editMenuItem'>Submit</button>"
-  document.getElementById("itemEditButton32").addEventListener('click', function() {
+  document.getElementById("itemEditButton32").addEventListener('click', function () {
     submitMenuEdit(index);
-      let toRemove = document.getElementsByClassName("editMenuItemPrompt")
-        toRemove[0].innerHTML = "<label>Submitted!</label>"
-        for(let i=1; i<toRemove.length; i++){
-            toRemove[i].innerHTML = ""
-        }
-        document.getElementById("nameEditPrompt").remove()
-        document.getElementById("priceEditPrompt").remove()
-        document.getElementById("caloriesEditPrompt").remove()
-        document.getElementById("itemEditButton32").remove()
+    let toRemove = document.getElementsByClassName("editMenuItemPrompt")
+    toRemove[0].innerHTML = "<label>Submitted!</label>"
+    for (let i = 1; i < toRemove.length; i++) {
+      toRemove[i].innerHTML = ""
+    }
+    document.getElementById("nameEditPrompt").remove()
+    document.getElementById("priceEditPrompt").remove()
+    document.getElementById("caloriesEditPrompt").remove()
+    document.getElementById("itemEditButton32").remove()
 
-    })
+  })
 
 
   // MUST REPLACE EDIT BUTTON TO BE CALLED SUBMIT AND MAKE IT CALL submitMenuEdit() FUNCTION
@@ -134,16 +143,16 @@ async function submitMenuEdit(index) {
 }
 
 // Functions to add and delete menu items
-function addMenuItem(){
-    let nameValue = document.getElementById("newItemNameField").value;
-    let priceValue = parseFloat(document.getElementById("newItemPriceField").value);
-    let caloriesValue = parseInt(document.getElementById("newItemCaloriesField").value);
-    addItemToDB(nameValue, priceValue, caloriesValue);
+function addMenuItem() {
+  let nameValue = document.getElementById("newItemNameField").value;
+  let priceValue = parseFloat(document.getElementById("newItemPriceField").value);
+  let caloriesValue = parseInt(document.getElementById("newItemCaloriesField").value);
+  addItemToDB(nameValue, priceValue, caloriesValue);
 }
 
-function deleteMenuItem(){
-    let nameValue = document.getElementById("deleteItemNameField").value;
-    removeItem(nameValue);
+function deleteMenuItem() {
+  let nameValue = document.getElementById("deleteItemNameField").value;
+  removeItem(nameValue);
 }
 
 // Add menu item
@@ -160,10 +169,10 @@ async function addItemToDB(name, _price, _calories) {
         calories: _calories
       })
     })
-     return 0
+    return 0
   } catch (error) {
     console.error(error)
-     return -1
+    return -1
   }
 }
 
@@ -188,7 +197,7 @@ async function removeItem(name) {
     }).toString(), {
       method: "DELETE"
     })
-return 0
+    return 0
   } catch (error) {
     console.error(error)
     return -1
@@ -241,11 +250,11 @@ async function requestMenu(userSearchTerm, userMaxPrice, userMaxCalories) {
 // Stores menu items in basket using cookies
 // Cookie structure is CSV in form: id,itemName,price,calories,quantity
 function addToBasket(index, itemId, itemName, price, calories) {
-  let quantity = document.getElementById("itemQuantityInput"+index).value
+  let quantity = document.getElementById("itemQuantityInput" + index).value
 
   // This will work for now as we only store 1 type of cookie
   let previousCookieContent = document.cookie.split("basket=")[1];
-  if(previousCookieContent == null){
+  if (previousCookieContent == null) {
     document.cookie = "basket="
     previousCookieContent = document.cookie.split("basket=")[1];
   }
@@ -257,75 +266,75 @@ function addToBasket(index, itemId, itemName, price, calories) {
   previousCookieContent.split("#").forEach(element => {
     let splitCookie = element.split(",")
     // Item found in basket
-    if(splitCookie[0] == itemId){
+    if (splitCookie[0] == itemId) {
       // Then update quantity instead
-      let newQuantity = Number(splitCookie[4])+Number(quantity);
-      
+      let newQuantity = Number(splitCookie[4]) + Number(quantity);
+
       let indexOfItem = previousCookieContent.indexOf(element)
       let indexOfEndOfItem = previousCookieContent.indexOf("#", indexOfItem)
-      let updatedCookieSegment = previousCookieContent.substring(indexOfItem, indexOfEndOfItem-1)+newQuantity
-      let updatedCookie = previousCookieContent.substring(0,indexOfItem)+updatedCookieSegment+previousCookieContent.substring(indexOfEndOfItem, previousCookieContent.length);
-      document.cookie = "basket="+updatedCookie;
+      let updatedCookieSegment = previousCookieContent.substring(indexOfItem, indexOfEndOfItem - 1) + newQuantity
+      let updatedCookie = previousCookieContent.substring(0, indexOfItem) + updatedCookieSegment + previousCookieContent.substring(indexOfEndOfItem, previousCookieContent.length);
+      document.cookie = "basket=" + updatedCookie;
 
       updated = true;
     }
   });
 
-  if(!updated){
-    document.cookie="basket="+itemId+","+itemName+","+price+","+calories+","+quantity+"#"+previousCookieContent;
+  if (!updated) {
+    document.cookie = "basket=" + itemId + "," + itemName + "," + price + "," + calories + "," + quantity + "#" + previousCookieContent;
 
     let previousBasket = document.getElementById("basketIcon").innerHTML;
-    let previousBasketQuantity = previousBasket.substring(previousBasket.length-1,previousBasket.length);
-    let newQuantity = Number(previousBasketQuantity)+1;
-    document.getElementById("basketIcon").innerHTML = "🛒 "+newQuantity;
+    let previousBasketQuantity = previousBasket.substring(previousBasket.length - 1, previousBasket.length);
+    let newQuantity = Number(previousBasketQuantity) + 1;
+    document.getElementById("basketIcon").innerHTML = "🛒 " + newQuantity;
   }
 
-  console.log("Current basket:"+document.cookie);
-  
+  console.log("Current basket:" + document.cookie);
+
 }
 
 //function to update basket icon with the item quantity in order
 function updateBasketIcon() {
- let order = JSON.parse(localStorage.getItem('order')) || [];
- let basketIcon = document.getElementById('basketIcon');
- let totalQuantity = order.reduce((total, item) => total + item.quantity, 0);
- basketIcon.textContent = `🛒 ${totalQuantity}`;
+  let order = JSON.parse(localStorage.getItem('order')) || [];
+  let basketIcon = document.getElementById('basketIcon');
+  let totalQuantity = order.reduce((total, item) => total + item.quantity, 0);
+  basketIcon.textContent = `🛒 ${totalQuantity}`;
 }
 
 //function ro remove menu item from the order
 function removeFromOrder(itemName) {
- let order = JSON.parse(localStorage.getItem('order')) || [];
- const itemIndex = order.findIndex(item => item.itemName === itemName);
- if (itemIndex >= 0) {
-   order.splice(itemIndex, 1);
-   localStorage.setItem('order', JSON.stringify(order));
- }
+  let order = JSON.parse(localStorage.getItem('order')) || [];
+  const itemIndex = order.findIndex(item => item.itemName === itemName);
+  if (itemIndex >= 0) {
+    order.splice(itemIndex, 1);
+    localStorage.setItem('order', JSON.stringify(order));
+  }
 }
 //updates order details and basket icon when DOM is loaded
-document.addEventListener('DOMContentLoaded', function() {
-  if(document.title.indexOf("Menu") != -1){
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.title.indexOf("Menu") != -1) {
     initializeMenu();
     initializeCategoryFilter();
     updateBasketIcon();
   }
 });
 
-function filterItems(){
-   let searchTerm = document.getElementById('searchTerm').value;
-   let maxCalories = parseInt(document.getElementById('maxCalories').value) || 0;
-   let maxPrice = parseFloat(document.getElementById('maxPrice').value) || 0;
-   if (searchTerm.length <= 0){
-       searchTerm = 0;
-   }
-   let data = requestMenu(searchTerm, maxPrice, maxCalories);
-   data.then(r =>{
-       let index = 0;
-       document.getElementById("MenuItemGridLayout").innerHTML = "";
-       r.forEach(element => {
-           document.getElementById("MenuItemGridLayout").innerHTML+= createMenuItem(index, element.itemName, element.price, element.calories);
-           index++;
-       });
-   })
+function filterItems() {
+  let searchTerm = document.getElementById('searchTerm').value;
+  let maxCalories = parseInt(document.getElementById('maxCalories').value) || 0;
+  let maxPrice = parseFloat(document.getElementById('maxPrice').value) || 0;
+  if (searchTerm.length <= 0) {
+    searchTerm = 0;
+  }
+  let data = requestMenu(searchTerm, maxPrice, maxCalories);
+  data.then(r => {
+    let index = 0;
+    document.getElementById("MenuItemGridLayout").innerHTML = "";
+    r.forEach(element => {
+      document.getElementById("MenuItemGridLayout").innerHTML += createMenuItem(index, element.itemName, element.price, element.calories);
+      index++;
+    });
+  })
 }
 
 function initializeMenu() {
@@ -345,24 +354,24 @@ function initializeMenu() {
 }
 
 // Initialises basket quantity on page load to number of items in basket cookies
-function initBasketQuantity(){
-  if(document.cookie.length <= 0){
-    document.cookie="basket="
+function initBasketQuantity() {
+  if (document.cookie.length <= 0) {
+    document.cookie = "basket="
   }
   let cookieList = document.cookie.split(";");
   let basketCookie = "";
   cookieList.forEach(cookie => {
-    if(cookie.indexOf("basket=")!=-1){
-      cookie = cookie.substring(cookie.indexOf("basket=")+"basket=".length, cookie.length)
+    if (cookie.indexOf("basket=") != -1) {
+      cookie = cookie.substring(cookie.indexOf("basket=") + "basket=".length, cookie.length)
       console.log(cookie)
       // we found basket cookie
       let splitCookie = cookie.split("#")
       let basketCount = splitCookie.length
-      if(splitCookie[splitCookie.length-1].length <= 0){
+      if (splitCookie[splitCookie.length - 1].length <= 0) {
         basketCount--;
       }
       console.log(basketCount);
-      document.getElementById("basketIcon").innerHTML="🛒 "+basketCount;
+      document.getElementById("basketIcon").innerHTML = "🛒 " + basketCount;
     }
   })
 }
