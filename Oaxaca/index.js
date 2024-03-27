364548a512b73130502443b0ed4df032666ece3b
Dawood <ZLAC187@live.rhul.ac.uk>
2024-03-27 10:58:30 +0000

    Fixed import issue

--- Oaxaca/index.js
+++ Oaxaca/index.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', e => {
+    initCookies();
+});
+
 // Called when page is loaded. This both initialises and resets cookies.
 function initCookies() {
     document.cookie = "tableNo=;";
